e920e99860727de71c9172a8b6e4e1e655e7ef31
Noitidart <noitidart@gmail.com>
2015-09-15 03:46:23 -0700

    1.0-night.rev1
    Ok set up gallery to show self image, working nicely

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -47,10 +47,54 @@ var	ANG_APP = angular.module('zooniversexpert', [])
 		gAngScope = gAngBody.scope();
 		// gAngInjector = gAngBody.injector();
 		
+		MODULE.imgExts = [
+			'JPG',
+			'PNG'
+		];
+		MODULE.imgExtSelected = 'JPG';
 		
+		MODULE.id = '6076';
+		
+		MODULE.id_length = '6';
+		
+		MODULE.url_prefix = 'http://zooniverse-export.s3-website-us-east-1.amazonaws.com/21484_1000_D35_Season%202_Set%201_EK';
+		
+		MODULE.gallery = [];
+		/* entries are objects
+			{
+				url: ''
+			}
+		*/
+		
+		var idPreceededByZeroes = function(aId) {
+			var str = aId + '';
+			while (str.length < MODULE.id_length) {
+				str = '0' + str;
+			}
+			return str;
+		}
+		
+		MODULE.generate = function() {
+			alert('start');
+			
+			MODULE.gallery = [];
+			
+			// push in target
+			MODULE.gallery.push(
+				{
+					url: MODULE.url_prefix + idPreceededByZeroes(MODULE.id) + '.' + MODULE.imgExtSelected
+				}
+			);
+			
+			alert(MODULE.url_prefix + idPreceededByZeroes(MODULE.id) + '.' + MODULE.imgExtSelected);
+			// find > id until find a broken image
+			// MODULE.gallery.
+			
+			alert('done');
+		}
 	}]);
 
 function onPageReady() {
-	alert(myServices.sb.GetStringFromName('welcome-msg'))
+	// alert(myServices.sb.GetStringFromName('welcome-msg'));
 }
 document.addEventListener('DOMContentLoaded', onPageReady, false);
